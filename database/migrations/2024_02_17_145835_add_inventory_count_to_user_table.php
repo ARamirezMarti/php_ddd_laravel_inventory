22435ecb36bb15106e26889ce6cef507d12133f7
Tony <tony@pop-os.localdomain>
2024-02-17 17:46:04 +0100

    Add user inventory count in users table

--- database/migrations/2024_02_17_145835_add_inventory_count_to_user_table.php
+++ database/migrations/2024_02_17_145835_add_inventory_count_to_user_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('inventories_count');
+            $table->integer('inventories_count')->default(0);
         });
     }
 
